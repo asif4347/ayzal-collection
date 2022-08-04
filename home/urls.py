ff890b9574574bfcf9d2d4080596f31c58ac87fe
Muhammad Asif <moh.asif4347@gmail.com>
2022-08-04 19:41:38 +0500

    Home url updated

--- home/urls.py
+++ home/urls.py
@@ -18,5 +18,5 @@ from django.urls import path,include
 
 urlpatterns = [
                   path('admin/', admin.site.urls),
-                  path('home/', include('ComingSoon.urls')),
+                  path('', include('ComingSoon.urls')),
 ]
